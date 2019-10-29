f8bb220e4a4b7a217611c03f0ac445b7f27f1416
info <info@info17.cshl.edu>
2019-10-28 20:24:44 -0400

    moved bus text files

--- animate_bus.py
+++ animate_bus.py
@@ -4,7 +4,7 @@ def moving_bus():
 	import os, time
 
 	os.system('clear')
-	filenames = ['bus7.txt','bus6.txt','bus5.txt','bus4.txt','bus3.txt','bus2.txt','bus1.5.txt','bus1.4.txt','bus1.3.txt','bus1.2.txt', 'bus1.txt', 'bus.txt']
+	filenames = ['ascii_art/bus7.txt','ascii_art/bus6.txt','ascii_art/bus5.txt','ascii_artbus4.txt','ascii_art/bus3.txt','acsii_art/bus2.txt','ascii_art/bus1.5.txt','ascii_art/bus1.4.txt','ascii_art/bus1.3.txt','ascii_art/bus1.2.txt', 'ascii_art/bus1.txt', 'ascii_art/bus.txt']
 
 	frames = []
 

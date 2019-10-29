ee56eda5ce946885360ebd92a465c2fc504d753e
info <info@info08.cshl.edu>
2019-10-28 20:41:53 -0400

    added random encounters, they still need pictures

--- Random_encouters.py
+++ Random_encouters.py
@@ -1,83 +1,156 @@
 #!/usr/bin/env python3
 import user
 import random
+import time
+import sys
+from print_quiz import re_input_easy, re_input_med, re_input_hard
 
 player = user.User('Horseshoe Crab', 100, 80, 50, 60, set(), dict())
 
 def random_en1(user):
 	#find sketch food, do you eat it?
+	#if you choose to eat the apple, there's a 50% chance it will injure you and a 50% chance it will heal you
 	#print apple art
 	print("You found an apple, just sitting there on the ground. It looks very appetizing, but it's kind of strange that someone left it here. What do you do?")
 	print("A) Eat the apple or B) Don't eat the apple")
-	en1_ans = input('>>>')
+	en1_ans = input('>>> ')
+	while en1_ans not in ['a','b']
+		print("There's only two choices, it's not that hard. Don't hurt yourself, try again.")
+		en1_ans = input('>>> ')
 	if en1_ans.lower() == 'a':
 		randomnum = random.randint(1,3)
 		if randomnum == 1:
 			print("Nasty! Why would you eat something you found on the ground? You lose 10 health")
 			user.injure(10)
+			user.print_health_bar()
 		else:
 			print("Yum, a crisp and juicy apple. You gain 10 health.")
 			user.heal(10)
-	elif en1_ans.lower() == 'b':
-		print("You leave the apple alone and walk away")
+			user.print_health_bar()
 	else:
-		print("What? Try again.")
-	#there's a 50% chance it was rotten, you lose health
-	#there's a 50% chance it was delicious, you gain health
+		print("You leave the apple alone and walk away")
 random_en1(player)
 player.print_user_stats()
 
 def random_en2(user):
-	#useless item encouter
-	print("A mysterious man approaches you."
-	time.sleep(1)
-	print("He hands you an ordinary-looking dirty pinecone.") 
-	print("'It's dangerous to go alone! Take this!'")
-	if 'items' not in user.inventory.keys():
-		user.inventory['items'] = list()
-		print("It's probably a good idea not to argue with him. You put the pinecone in your inventory and walk away.")
-		user.add_item('Useless Pinecone')
-		print(user.inventory['items']) 
-	elif 'Useless Pinecone' in user.inventory['items']:
-		print("Yikes...this guy again. You take the pinecone and discreetly throw it out.")
+  #useless item encouter
+  print("A mysterious man approaches you.")
+  time.sleep(2)
+  #print man image
+  print("He hands you an ordinary-looking dirty pinecone.")
+  time.sleep(2)
+  #print pinecone image
+  print("'It's dangerous to go alone! Take this!'")
+  if 'Useless Pinecone' in user.inventory['items']:
+    print("Yikes...this guy again. You take the pinecone and discreetly throw it out.")
+  else: 
+    time.sleep(1)
+    delay_string = ("...")
+    for char in delay_string:
+      sys.stdout.write(char)
+      sys.stdout.flush()
+      time.sleep(1)
+    time.sleep(2)
+    print("\n""Okay...It's probably a good idea not to argue with him. You put the pinecone in your inventory and walk away.")
+    user.add_item('Useless Pinecone')
+random_en2(player)
+
+def random_en3(user):
+  print("Shasta the Sphinx walks up to you.")
+  #text color change
+  print("\033[32mAnswer my questions and I will give you a prize!\033[m")
+  if user.intel >= 65:
+    print("You're a smart cookie, this one will be easy!")
+    re_e = re_input_easy('')
+    #easy regular expression quiz
+    while re_e.lower() not in ['a','b','c','d']:
+      print("That makes no sense. What did you say?")
+      re_e = re_input_easy('')
+    if re_e.lower() == 'b':
+      print("Congrats! You chose the correct answer. All that studying is paying off.")
+      print("Shasta hands you a health potion. Sweet!")
+      user.add_item('Health Potion')
+    else:
+      re_e.lower() == ('a' or 'c' or 'd')
+      print("Yikes...that's not right. Were you asleep during that module?")
+      print("You walk away empty-handed")
+	elif user.intel >= 50:
+    #medium regex quiz
+    print("Your intelligence is okay, but you'll have to think a little on this one...")
+    re_m = re_input_med('')
+    while re_m.lower() not in ['a','b','c','d']:
+      print("That makes no sense. What did you say?")
+      re_m = re_input_med('')
+    if re_m.lower() == 'b':
+      print("Congrats! You chose the correct answer. All that studying is paying off.")
+      print("Shasta hands you a health potion. Sweet!")
+      user.add_item('Health Potion')
+    else:
+      print("Yikes...that's not right. Were you asleep during that module?")
+      print("You walk away empty-handed")
 	else:
-		print("It's probably a good idea not to argue with him. You put the pinecone in your inventory and walk away.") 
-		user.add_item('Useless Pinecone')
-		print("Inventory:", user.inventory['items']) 
-random_en2(player)  
+    #hard regex quiz
+    print("Whew, you're not feeling so smart right now. This is going to be a tough one.")
+    re_h = re_input_hard('')
+    while re_h.lower() not in ['a','b','c','d']:
+      print("That makes no sense. What did you say?")
+      re_h = re_input_hard('')
+    if re_h.lower() == 'b':
+      print("Congrats! You chose the correct answer. All that studying is paying off.")
+      print("Shasta hands you a health potion. Sweet!")
+      user.add_item('Health Potion')
+    else:
+      print("Yikes...that's not right. Were you asleep during that module?")
+      print("You walk away empty-handed")
+random_en3(player)
 
-
-
-
-#make mini-encounter before racoon fight, yields healing potion
-def random_en3(self):
-	print("Someone walks up to you.")
-	#text color change
-	print("You'll probably need this.")
-	print("She hands you a free health potion. Sweet!")
-	player.add_item("Health Potion")
-
-
-
-
-
+#make mini-encounter before swan fight, yields "Bread"
+def random_en4(user):
+  print("Joe walks by and offers you some bread from the cafeteria. It looks like normal bread, but why is he giggling? Do you take the bread?")
+  print("A) Yes or  B) No")
+  en4_ans = input(">>> ")
+  print(en4_ans)
+  while en4_ans.lower() not in ['a','b']:
+    print("You have two options, A or B, it's not that hard. Try again.")
+    print("Joe walks by and offers you some bread from the cafeteria. It looks like normal bread, but why is he giggling? Do you take the bread?")
+    print("A) Take the bread or B) Just walk away")
+    en4_ans = input(">>> ")
+  if en4_ans.lower() == 'a':
+    user.add_item("Bread")
+    print("It seems like totally normal bread. This could be a nice snack in a little while...")
+    print("Do you:  A) eat the bread now or B) save it for later?")
+    bread_eat = input(">>> ")
+    while bread_eat.lower() not in ['a','b']:
+      print("You only have two choices, it's not that hard. Try again.")
+      print("It seems like totally normal bread. This could be a nice snack in a little while...")
+      print("Do you:  A) eat the bread now or B) save it for later?")
+      bread_eat = input(">>> ")
+    if bread_eat.lower() == 'a':
+        user.heal(10)
+        print("Yummy! The bread has restored 10 of your health points.")
+        user.print_health_bar()
+    else:
+        print("You keep the bread in your pocket for later, just in case.")
+  else:
+    print("Mom said never to take candy from strangers. You walk away from Joe.")
+random_en4(player)
 
 
 #this is redundant, take it out or re-skin it
 #Matty 2-soups encounter
-def random_en4(self):
-	print("A sudden fog descends. From the mist, MATTY 2-SOUPS emerges")
-	print("He offers you one of his soups. Which do you take?")
-	print("A) Chicken Noodle or B) Tomato bisque")
-	answer = input('>>>')
-	if en4_ans.lower() == 'a':
-		print("Delicious! So much soup, a naps seems nice right now... +10 health -5 speed")
-		player.speed(-5)
-		player.heal(10)
-	elif en4_ans.lower() == 'b':
-		print("Hey, this wasn't tomato bisque! It was some sort of magic sludge... +5 intelligence -5 health")
-	else:
-		print("Don't mess around with MATTY 2-SOUPS....which do you want?")
+#def random_en4(self):
+#	print("A sudden fog descends. From the mist, MATTY 2-SOUPS emerges")
+#	print("He offers you one of his soups. Which do you take?")
+#	print("A) Chicken Noodle or B) Tomato bisque")
+#	answer = input('>>>')
+#	if en4_ans.lower() == 'a':
+#		print("Delicious! So much soup, a naps seems nice right now... +10 health -5 speed")
+#		player.speed(-5)
+#		player.heal(10)
+#	elif en4_ans.lower() == 'b':
+#		print("Hey, this wasn't tomato bisque! It was some sort of magic sludge... +5 intelligence -5 health")
+#	else:
+#		print("Don't mess around with MATTY 2-SOUPS....which do you want?")
 	print(".")
 	time.sleep(1) 
 	print(".")
@@ -87,12 +160,7 @@ def random_en4(self):
 	print("Hmmm... MATTY 1-SOUP still has his remaining soup. He seems kinda attached to it though. Should you try to take it from him?")
 	
 
-
-
-
-
-#coffee encounter +speed, repeat
-
+#coffee encounter +speed, repeat?
 
 
 
